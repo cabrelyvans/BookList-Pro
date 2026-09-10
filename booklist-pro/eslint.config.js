06be8da7a2081668aebfe4864cea917eb9f588cc
cabrelyvans <dhscabrel@gmail.com>
2026-09-10 14:59:44 +0200

    chore:	bootstrap	Expo	Router	+ TypeScript	strict	+	ESLint/Prettier

---
+++ booklist-pro/eslint.config.js
@@ -0,0 +1,21 @@
+const expoConfig = require('eslint-config-expo/flat');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  ...expoConfig,
+  prettierConfig,
+  {
+    ignores: ['dist/*', 'node_modules/*', '.expo/*'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+  {
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+];
